44be40850f23cfee9a4443d5530a1f2d9895cab0
渔樵 <18852897377@163.com>
2018-12-02 14:26:46 +0800

    爬去蛋壳

--- src/test/java/yuqiao/housesearch/HouseSearchApplicationTests.java
+++ src/test/java/yuqiao/housesearch/HouseSearchApplicationTests.java
@@ -5,12 +5,24 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import yuqiao.housesearch.craw.DanKeCrawService;
+import yuqiao.housesearch.house.entity.House;
+import yuqiao.housesearch.house.service.IHouseService;
+import yuqiao.housesearch.house.service.impl.HouseServiceImpl;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HouseSearchApplicationTests {
+    @Autowired
+    private DanKeCrawService danKeCrawService;
     @Test
     public void contextLoads() {
+        List<House> houses=danKeCrawService.getHouseInfo("hz");
+        houses.forEach(house -> {
+            System.out.println(house);
+        });
     }
 
 }
